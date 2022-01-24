a0eb3d19854765fbaa7a9bb7aa9ba60e05f176a1
tomitheninja <suditomi@pm.me>
2022-01-24 18:54:11 +0100

    Improve avatar widget

--- lib/app/widgets/avatar.dart
+++ lib/app/widgets/avatar.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 String gravatar(String email) =>
     'https://www.gravatar.com/avatar/${md5.convert(utf8.encode(email)).toString()}?d=mp';
@@ -17,14 +18,23 @@ class Avatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return overrideImage != null
-        ? CircleAvatar(
-            radius: 64,
-            backgroundImage: overrideImage!,
-          )
-        : CircleAvatar(
-            radius: 64,
-            backgroundImage: image,
-          );
+    return GestureDetector(
+      onLongPressStart: (_) async {
+        await Get.dialog(Dialog(
+          child: overrideImage != null
+              ? Image(image: overrideImage!)
+              : Image(image: image),
+        ));
+      },
+      child: overrideImage != null
+          ? CircleAvatar(
+              radius: 64,
+              backgroundImage: overrideImage!,
+            )
+          : CircleAvatar(
+              radius: 64,
+              backgroundImage: image,
+            ),
+    );
   }
 }
--- lib/core/utils/pick_image.dart
+++ lib/core/utils/pick_image.dart
@@ -4,7 +4,13 @@ import 'package:image_picker/image_picker.dart';
 Future<Uint8List?> pickImage(ImageSource source) async {
   final ImagePicker _imagePicker = ImagePicker();
 
-  XFile? _file = await _imagePicker.pickImage(source: source);
+  XFile? _file = await _imagePicker.pickImage(
+    source: source,
+    maxWidth: 500,
+    maxHeight: 500,
+    imageQuality: 50,
+    preferredCameraDevice: CameraDevice.front,
+  );
   if (_file != null) {
     return await _file.readAsBytes();
   }
