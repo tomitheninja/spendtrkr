9db88aaed675734f5dbfff97a46ceeb3251ea465
tomitheninja <suditomi@pm.me>
2022-01-25 19:25:07 +0100

    Make transaction scrollable

--- lib/app/modules/transaction/page.dart
+++ lib/app/modules/transaction/page.dart
@@ -83,155 +83,159 @@ class TransactionUI extends StatelessWidget {
         ),
         body: SafeArea(
           child: Form(
-            child: Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 16),
-                  color: context.theme.cardColor,
-                  child: TextFormField(
-                    onChanged: (x) {
-                      title.value = x;
-                    },
-                    initialValue: title.value,
-                    style: context.theme.textTheme.headline5!
-                        .copyWith(backgroundColor: context.theme.cardColor),
-                    decoration: InputDecoration(
-                      hintText: 'transaction.title-hint'.tr,
-                      border: InputBorder.none,
-                      labelText: 'transaction.title-label'.tr,
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: 4, horizontal: 16),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    color: context.theme.cardColor,
+                    child: TextFormField(
+                      onChanged: (x) {
+                        title.value = x;
+                      },
+                      initialValue: title.value,
+                      style: context.theme.textTheme.headline5!
+                          .copyWith(backgroundColor: context.theme.cardColor),
+                      decoration: InputDecoration(
+                        hintText: 'transaction.title-hint'.tr,
+                        border: InputBorder.none,
+                        labelText: 'transaction.title-label'.tr,
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 4, horizontal: 16),
+                      ),
                     ),
                   ),
-                ),
-                Column(children: [
-                  const SizedBox(height: 25),
-                  TextFormField(
-                    onChanged: (x) {
-                      try {
-                        amount.value = double.parse(x);
-                      } catch (_) {}
-                    },
-                    initialValue: (amount.value.toInt() == amount.value
-                            ? amount.toInt()
-                            : amount)
-                        .toString(),
-                    keyboardType: TextInputType.number,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.allow(RegExp(r'[0-9.]')),
-                    ],
-                    decoration: InputDecoration(
-                      hintText: 'transaction.amount-hint'.tr,
-                      border: const OutlineInputBorder(),
-                      labelText: 'transaction.amount'.tr,
-                      contentPadding: const EdgeInsets.all(24),
-                      prefixIcon: const Icon(Icons.attach_money),
+                  Column(children: [
+                    const SizedBox(height: 25),
+                    TextFormField(
+                      onChanged: (x) {
+                        try {
+                          amount.value = double.parse(x);
+                        } catch (_) {}
+                      },
+                      initialValue: (amount.value.toInt() == amount.value
+                              ? amount.toInt()
+                              : amount)
+                          .toString(),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9.]')),
+                      ],
+                      decoration: InputDecoration(
+                        hintText: 'transaction.amount-hint'.tr,
+                        border: const OutlineInputBorder(),
+                        labelText: 'transaction.amount'.tr,
+                        contentPadding: const EdgeInsets.all(24),
+                        prefixIcon: const Icon(Icons.attach_money),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  DateTimeFormField(
-                    initialDate: date.value,
-                    onSaved: (x) {
-                      if (x != null) {
-                        date.value = x;
-                      }
-                    },
-                    decoration: InputDecoration(
-                      hintStyle: const TextStyle(color: Colors.black45),
-                      errorStyle: const TextStyle(color: Colors.redAccent),
-                      border: const OutlineInputBorder(),
-                      contentPadding: const EdgeInsets.all(34),
-                      prefixIcon: const Icon(Icons.event_note),
-                      labelText: 'transaction.pick-time'.tr,
+                    const SizedBox(height: 16),
+                    DateTimeFormField(
+                      initialDate: date.value,
+                      onSaved: (x) {
+                        if (x != null) {
+                          date.value = x;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        hintStyle: const TextStyle(color: Colors.black45),
+                        errorStyle: const TextStyle(color: Colors.redAccent),
+                        border: const OutlineInputBorder(),
+                        contentPadding: const EdgeInsets.all(34),
+                        prefixIcon: const Icon(Icons.event_note),
+                        labelText: 'transaction.pick-time'.tr,
+                      ),
+                      mode: DateTimeFieldPickerMode.date,
+                      autovalidateMode: AutovalidateMode.always,
+                      onDateSelected: (DateTime value) {
+                        date.value = value;
+                      },
                     ),
-                    mode: DateTimeFieldPickerMode.date,
-                    autovalidateMode: AutovalidateMode.always,
-                    onDateSelected: (DateTime value) {
-                      date.value = value;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  TextButton(
-                    onPressed: () async {
-                      debugPrint('tap');
-                      final FlutterContactPicker _contactPicker =
-                          FlutterContactPicker();
-                      Contact? _contact = await _contactPicker.selectContact();
+                    const SizedBox(height: 16),
+                    TextButton(
+                      onPressed: () async {
+                        debugPrint('tap');
+                        final FlutterContactPicker _contactPicker =
+                            FlutterContactPicker();
+                        Contact? _contact =
+                            await _contactPicker.selectContact();
 
-                      if (_contact != null) {
-                        if (_contact.phoneNumbers != null) {
-                          if (_contact.phoneNumbers!.isNotEmpty) {
-                            contact.value = ((_contact.fullName ?? '') + ' ') +
-                                _contact.phoneNumbers!.first;
+                        if (_contact != null) {
+                          if (_contact.phoneNumbers != null) {
+                            if (_contact.phoneNumbers!.isNotEmpty) {
+                              contact.value =
+                                  ((_contact.fullName ?? '') + ' ') +
+                                      _contact.phoneNumbers!.first;
+                            }
                           }
                         }
-                      }
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Icon(Icons.contacts),
-                        Text(
-                          contact.value.isEmpty
-                              ? 'transaction.select-contact'.tr
-                              : contact.value,
-                          style: context.theme.textTheme.headline5!,
-                        ),
-                        const SizedBox.shrink(),
-                      ],
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Icon(Icons.contacts),
+                          Text(
+                            contact.value.isEmpty
+                                ? 'transaction.select-contact'.tr
+                                : contact.value,
+                            style: context.theme.textTheme.headline5!,
+                          ),
+                          const SizedBox.shrink(),
+                        ],
+                      ),
+                      /* decoration: const InputDecoration(
+                        hintText: 'Pick location',
+                        border: OutlineInputBorder(),
+                        labelText: 'location',
+                        contentPadding: EdgeInsets.all(24),
+                        prefixIcon: Icon(Icons.pin_drop),
+                      ), */
                     ),
-                    /* decoration: const InputDecoration(
-                      hintText: 'Pick location',
-                      border: OutlineInputBorder(),
-                      labelText: 'location',
-                      contentPadding: EdgeInsets.all(24),
-                      prefixIcon: Icon(Icons.pin_drop),
-                    ), */
-                  ),
-                  const SizedBox(height: 16),
-                  TextButton(
-                    onPressed: () async {
-                      final ImagePicker _imagePicker = ImagePicker();
-                      XFile? _file = await _imagePicker.pickImage(
-                        source: ImageSource.gallery,
-                      );
-                      if (_file != null) {
-                        final bytes = await _file.readAsBytes();
-                        final img = await FirebaseStorage.instance
-                            .ref('transactions/$ownerId')
-                            .child(DateTime.now().hashCode.toString())
-                            .putData(bytes);
-                        photoUrl.value = await img.ref.getDownloadURL();
-                      }
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Icon(Icons.camera_alt),
-                        Text(
-                          'transaction.select-camera'.tr,
-                          style: context.theme.textTheme.headline5!,
-                        ),
-                        const SizedBox.shrink(),
-                      ],
+                    const SizedBox(height: 16),
+                    TextButton(
+                      onPressed: () async {
+                        final ImagePicker _imagePicker = ImagePicker();
+                        XFile? _file = await _imagePicker.pickImage(
+                          source: ImageSource.gallery,
+                        );
+                        if (_file != null) {
+                          final bytes = await _file.readAsBytes();
+                          final img = await FirebaseStorage.instance
+                              .ref('transactions/$ownerId')
+                              .child(DateTime.now().hashCode.toString())
+                              .putData(bytes);
+                          photoUrl.value = await img.ref.getDownloadURL();
+                        }
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Icon(Icons.camera_alt),
+                          Text(
+                            'transaction.select-camera'.tr,
+                            style: context.theme.textTheme.headline5!,
+                          ),
+                          const SizedBox.shrink(),
+                        ],
+                      ),
+                      /* decoration: const InputDecoration(
+                        hintText: 'Pick location',
+                        border: OutlineInputBorder(),
+                        labelText: 'location',
+                        contentPadding: EdgeInsets.all(24),
+                        prefixIcon: Icon(Icons.pin_drop),
+                      ), */
                     ),
-                    /* decoration: const InputDecoration(
-                      hintText: 'Pick location',
-                      border: OutlineInputBorder(),
-                      labelText: 'location',
-                      contentPadding: EdgeInsets.all(24),
-                      prefixIcon: Icon(Icons.pin_drop),
-                    ), */
-                  ),
-                  const SizedBox(height: 16),
-                  photoUrl.value.isNotEmpty
-                      ? Image.network(
-                          photoUrl.value,
-                          fit: BoxFit.cover,
-                        )
-                      : const SizedBox.shrink(),
-                ]),
-              ],
+                    const SizedBox(height: 16),
+                    photoUrl.value.isNotEmpty
+                        ? Image.network(
+                            photoUrl.value,
+                            fit: BoxFit.cover,
+                          )
+                        : const SizedBox.shrink(),
+                  ]),
+                ],
+              ),
             ),
           ),
         ),
